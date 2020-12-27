470c8505472b150f4e55f4d24b44683588bf8947
username <54479676+CormacKrum@users.noreply.github.com>
2020-12-27 12:52:25 +0530

    Initial Commit

---
+++ Day 2 Loops.js
@@ -0,0 +1,25 @@
+
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+    for (var i = 0; i < s.length; i++)
+    {
+        var x = s.charAt(i);
+        if (x=='a'||x=='e'||x=='i'||x=='o'||x=='u')
+        {
+            console.log(s.charAt(i));
+        }
+    }
+    for (var i = 0; i < s.length; i++)
+    {
+        var x = s.charAt(i);
+        if (x != 'a' && x != 'e' && x != 'i' && x != 'o' && x != 'u')
+        {
+            console.log(s.charAt(i));
+        }
+    }
+}
+

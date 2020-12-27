a22d16ac99fc5a1a147c982b9d942847a06d614b
username <54479676+CormacKrum@users.noreply.github.com>
2020-12-27 12:50:49 +0530

    Initial Commit

---
+++ 1D Arrays in C.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n;
+    scanf("%d",&n);
+    int ar[n];
+    int i;
+    int sum=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+        sum=sum+ar[i];
+    }
+    printf("%d",sum);
+    return 0;
+}
+

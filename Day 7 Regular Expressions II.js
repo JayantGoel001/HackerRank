d656d88307c110dd48f8c060f8bf2bf96bb902c8
username <54479676+CormacKrum@users.noreply.github.com>
2020-12-27 14:05:09 +0530

    Initial Commit

--- Day 7 Regular Expressions II.js
+++ Day 7 Regular Expressions II.js
@@ -1,11 +1,6 @@
 
 
 function regexVar() {
-    /*
-     * Declare a RegExp object variable named 're'
-     * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
-     * followed by one or more letters.
-     */
     const re=/^(Mr|Mrs|Ms|Dr|Er)\.[a-zA-Z]+$/
     
     /*
@@ -13,4 +8,3 @@ function regexVar() {
      */
     return re;
 }
-

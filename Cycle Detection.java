dcae86eb0f81914f4b5b564a7ba1a6cbbee1f737
username <54479676+CormacKrum@users.noreply.github.com>
2020-12-27 13:37:07 +0530

    Initial Commit

--- Cycle Detection.java
+++ Cycle Detection.java
@@ -1,28 +1,14 @@
-
-
-    // Complete the hasCycle function below.
-
-    /*
-     * For your reference:
-     *
-     * SinglyLinkedListNode {
-     *     int data;
-     *     SinglyLinkedListNode next;
-     * }
-     *
-     */
-    static boolean hasCycle(SinglyLinkedListNode head) 
+static boolean hasCycle(SinglyLinkedListNode head) 
+{
+    int i=0;
+    while(i<=1000)
     {
-        int i=0;
-        while(i<=1000)
+        if(head==null)
         {
-            if(head==null)
-            {
-                return false;
-            }
-            head=head.next;
-            i++;
+            return false;
         }
-        return true;
+        head=head.next;
+        i++;
     }
-
+    return true;
+}
